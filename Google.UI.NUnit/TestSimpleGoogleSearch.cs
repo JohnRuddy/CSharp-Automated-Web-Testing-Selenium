3e0a47b8eb909a50545d66f145eecb1ba84fc592
John Ruddy <johnpruddy@gmail.com>
2014-10-23 10:54:46 +0100

    First Commit

---
+++ Google.UI.NUnit/TestSimpleGoogleSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Chrome;
+using System.Drawing.Imaging;
+
+namespace Google.UI.NUnit
+{
+
+    [TestFixture]
+    public class Test
+    {
+        private IWebDriver _driver;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver.Dispose();
+            }
+        }
+
+        [Test]
+        public void TestSimpleGoogleWebSearch()
+        {
+            _driver.Navigate().GoToUrl(
+              "http://www.google.com"
+            );
+
+            ((ITakesScreenshot)_driver).GetScreenshot().SaveAsFile("Step.1 - Start Page.jpg", ImageFormat.Jpeg);
+
+            _driver.FindElement(By.Name("q")).SendKeys("1 + 1");
+            _driver.FindElement(By.Id("gbqfb")).Submit();
+
+
+            ((ITakesScreenshot)_driver).GetScreenshot().SaveAsFile("Step.2 - Results Page.jpg", ImageFormat.Jpeg);
+
+            Assert.That(_driver.Title.Contains("1 + 1 - Google Search"));
+        }
+    }
+}
